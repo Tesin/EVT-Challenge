dfb5d317f6810face2e2815e160e7b6f425dd726
Sean Brooks <sean@the-architect.io>
2023-04-19 14:29:08 -0500

    [EVT-006] Save point.

--- components/DesignHeader.tsx
+++ components/DesignHeader.tsx
@@ -4,7 +4,26 @@ const DesignHeader: React.FC = () => {
   return (
     <div className={classes.designHeader}>
       <h1 className={classes.title}>Design Decisions</h1>
-      <p className={classes.introText}>Stuff</p>
+      <p className={classes.introText}>
+        When I first read the assignment there was one aspect that really stood
+        out to me:
+      </p>
+      <div className={classes.blockquoteContainer}>
+        <blockquote className={classes.blockquote}>
+          Producing a fully automated deployment mechanism for any application
+          assumes a huge number of things, particularly how the dev team will be
+          interacting with this thing. <p className={classes.author}>- Me</p>
+        </blockquote>
+      </div>
+      <div className={classes.introText}>
+        <p>
+          So the first decision I made was to make an actual application
+          because:
+          <ul>
+            <li>hi</li>
+          </ul>
+        </p>
+      </div>
     </div>
   );
 };
